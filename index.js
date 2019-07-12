915ea6e7aa77df3b381a92b759ba0e63c9687622
fadhilahade <dhila.alkautsar@gmail.com>
2019-07-12 14:59:26 +0700

    add process env port

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express()
 
 app.get('/', (req, res, next) => res.send('Welcome!'))
 
-app.get('/hello', (request, response, next) => {
-    response.send({
+app.get('/hello', (req, res, next) => {
+    res.send({
         message: 'Hello world!'
     })
 })
 
-app.listen(3002, () => console.log('Express server is ready on localhost:3002'))
+app.post('/',(req,res,next) => res.send('Hello World'))
 
+app.listen(process.env.PORT || 3002, () => console.log('Express server is ready on localhost:3002'))
